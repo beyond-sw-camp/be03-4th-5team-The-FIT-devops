cc43a577a55319be37e5c68de669933a69effd08
kwondohoon1 <kwondohoon1@naver.com>
2024-02-06 18:14:55 +0900

    init project

---
+++ TheFit/src/main/java/com/example/TheFit/TheFitApplication.java
@@ -0,0 +1,13 @@
+package com.example.TheFit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheFitApplication.class, args);
+	}
+
+}
